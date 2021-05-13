4f4e2862faf2697496effe6a43246a80049c4fda
pitamer <itamar@codicate.com>
2021-05-13 20:35:29 +0300

    :tada: Initialize the project, add a route, start building the skeleton

---
+++ src/views/Audioepic.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="audioepic">
+    <h1>This is Audioepic</h1>
+    <h6>
+      Well, of course it's not very impressive <strong>now</strong>. But wait and see!
+    </h6>
+    <div id="tabs-section">
+      <div id="tabs">
+        <div class="tab">[]</div>
+        <div class="tab">[]</div>
+        <div class="tab">[]</div>
+      </div>
+      <div id="add-tab-button">
+        +
+      </div>
+    </div>
+    <div id="main-section">
+      <div id="sounds">
+        <div class="sound">~</div>
+        <div class="sound">~</div>
+        <div class="sound">~</div>
+      </div>
+      <div id="loops">
+        <div class="loop">o</div>
+        <div class="loop">o</div>
+        <div class="loop">o</div>
+      </div>
+      <div id="tracks">
+        <div class="track">#</div>
+        <div class="track">#</div>
+        <div class="track">#</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+#audioepic {
+  // height: 100%;
+  border: 1px red dashed;
+
+  // #tabs-section {
+  //   display: flex;
+
+  //   #tabs {
+  //     display: flex;
+  //   }
+  // }
+}
+</style>
